8ce4681e5b7a23f99270613c8b6df42aae8380d6
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-21 09:17:34 +0530

    init backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app =express();
+
+app.listen(5000, console.log(
+    "Server running on port 5000"
+))
+
+
+app.get('/',(req,res)=>{
+    res.send("API is running");
+})
+
